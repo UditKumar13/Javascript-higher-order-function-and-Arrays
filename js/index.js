14e4993ed735691f710c18687be36ac4c6dd32f7
Uditkumar13 <uditkumar2401@gmail.com>
2021-05-22 01:19:19 +0530

    filter usage

--- js/index.js
+++ js/index.js
@@ -14,21 +14,54 @@ const companies= [
   
   const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
   // standard for loop
-  
+  /*
   for ( let i=0 ; i<companies.length;i++){
       console.log(i);
       console.log(companies[i]);
   }
-
   // for each loop
 
   companies.forEach(function(company){
-      console.log(company);
+      console.log(company.name);
   });
 
+  */
+
+  // filter :
+
+ // 1.by simple for loop 
+  /*let canDrink = [];
   
+  for (let i=0;i<ages.length;i++){
+      if (ages[i] >=21) {
+          canDrink.push(ages[i]); 
+      }
+  } 
+  */
+// 2. By using the filter 
+
+const canDrink = ages.filter(function(age){
+   if (age >=21){
+       return true;
+   }
+})
+  console.log(canDrink);
+
+// works with the arrow function es6 
+const manDrink = ages.filter(age=> age>=21);
+console.log(manDrink);
 
 
+//use filter on deciding which companies are retailed one 
+//Es5 
+const retailCompanies = companies.filter (function(company){
+if (company.category === 'Retail'){
+    return true;
+}
+});
+
+console.log("Retailed companies");
+console.log(retailCompanies)
   
 
 
